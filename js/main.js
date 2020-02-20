9521b309eac7983cebc118ff1a4518b21e4247f4
Christian Beretta <beretta.christian2602@gmail.com>
2020-02-20 14:24:23 +0100

    correzione script

--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@
 // tramite ciclo for inserisco tutti i numeri da 1 a 100
 // tramite if distinguo i multipli di 3, 5, e di 3 e 5, a cui sostituisco il valore Fizz, Buzz, o FizzBuzz.
 var array = [];
-for (var i = 0; i < 100; i++) {
-    array[i] = i + 1;
-    if ((array[i] % 3) == 0) {
-        array[i] = 'Fizz';
+for (var i = 1; i < 100; i++) {
+    array[i] = i;
+    if ((array[i] % 5) == 0 && (array[i] % 3) == 0) {
+        array[i] = 'FizzBuzz';
     } else if ((array[i] % 5) == 0) {
         array[i] = 'Buzz';
-    } else if (((array[i] % 5) == 0) && ((array[i] % 3) == 0)) {
-        array[i] = 'FizzBuzz';
+    } else if ((array[i] % 3) == 0) {
+        array[i] = 'Fizz';
     }
 }
 
-document.getElementById('array-fizzbuzz').innerHTML = array;
+console.log(array);
